f652c1df5c6202e06af784c686d6aed8786c96bd
Nikhil Jha <reversecold@gmail.com>
2015-06-28 09:29:35 -0700

    Build Test File
    Built test files.

---
+++ atom.java
@@ -0,0 +1,8 @@
+// This is a test of Atom text editor.
+// If it works well - I'll use it in place of eclipse.
+
+public class Atom {
+  public static void main(String args[]) {
+    System.out.println("Hello, Atom!");
+  }
+}
