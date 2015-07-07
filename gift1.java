eb5b4ab4ef7581766e036141507285cf33322118
Nikhil Jha <reversecold@gmail.com>
2015-07-06 17:19:52 -0700

    Add GGG

--- gift1.java
+++ gift1.java
@@ -1,25 +1,56 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
+/*
+ID: nikhilf1
+LANG: JAVA
+TASK: gift1
+ */
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.StringTokenizer;
-
-
-public class gift1 {
-	  public static void main (String [] args) throws IOException {
-		    // Use BufferedReader rather than RandomAccessFile; it's much faster
-		    BufferedReader f = new BufferedReader(new FileReader("test.in"));
-		                                                  // input file name goes above
-		    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("test.out")));
-		    // Use StringTokenizer vs. readLine/split -- lots faster
-		    StringTokenizer st = new StringTokenizer(f.readLine());
-								  // Get line, break into tokens
-		    int i1 = Integer.parseInt(st.nextToken());    // first integer
-		    int i2 = Integer.parseInt(st.nextToken());    // second integer
-		    out.println(i1+i2);                           // output result
-		    out.close();                                  // close the output file
-		    System.exit(0);                               // don't omit this!
-		  }
-		}
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+ 
+class gift1 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(new File("gift1.in"));
+        PrintWriter pw = new PrintWriter(new File("gift1.out"));
+ 
+        int np = sc.nextInt();
+        List<String> people = new ArrayList<String>();
+        for (int i = 0; i < np; i++) {
+            people.add(sc.next());
+        }
+ 
+        Map<String, Integer> received = new HashMap<String, Integer>();
+        for (String person : people) {
+            received.put(person, 0);
+        }
+        Map<String, Integer> initial = new HashMap<String, Integer>();
+ 
+        for (int i = 0; i < np; i++) {
+            String person = sc.next();
+            int amount = sc.nextInt();
+            int recipients = sc.nextInt();
+ 
+            initial.put(person, amount);
+ 
+            int giftAmount = 0;
+            if (recipients > 0) {
+                giftAmount = amount / recipients;
+                received.put(person, received.get(person) + amount % recipients);
+            }
+            for (int j = 0; j < recipients; j++) {
+                String recipient = sc.next();
+                received.put(recipient, received.get(recipient) + giftAmount);
+            }
+        }
+ 
+        for (String person : people) {
+            pw.println(person + " " + (received.get(person) - initial.get(person)));
+        }
+ 
+        pw.close();
+    }
+}
